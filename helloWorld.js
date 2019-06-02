e544659cc2c4c8dbd3e583848bb977e7fd7f8ed3
Marcellus <nabbotsio@yahoo.com>
2019-06-02 15:32:07 +0200

    added goodbye function

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,12 @@
+
 const helloWorld = function() {
   return 'Hello world!';
 };
 
 console.log(helloWorld());
+
+const goodbyeWorld = function() {
+  return 'Goodbye world!';
+};
+
+console.log(goodbyeWorld());
